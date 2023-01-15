f0969365d6498924de252b5b84c3dc38a7f23bcb
youn1on <a.shymanska.ip13@kpi.ua>
2023-01-15 19:59:42 +0200

    Основнi сутностi створено

---
+++ bag_item.py
@@ -0,0 +1,5 @@
+class BagItem:
+    def __init__(self, num, value, weight):
+        self.number = num
+        self.value = value
+        self.weight = weight
---
+++ creature.py
@@ -0,0 +1,42 @@
+from bag_item import BagItem
+
+
+class Creature:
+    def __init__(self, chromosome: list[bool], item_set: list[BagItem]):
+        self.chromosome = chromosome
+        self.item_set = item_set
+        self.value = sum([i.value for (i, g) in zip(item_set, chromosome) if g])
+        self.weight = sum([i.weight for (i, g) in zip(item_set, chromosome) if g])
+
+    def crossover(self, another_creature, weight_limit):
+        pass
+
+    def mutation_at(self, pos: int):
+        if 0 > pos > len(self.chromosome):
+            raise Exception()
+        new_chromosome = [g for g in self.chromosome]
+        new_chromosome[pos] = not self.chromosome[pos]
+        nc = Creature(new_chromosome, self.item_set)
+        return nc
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    def __str__(self):
+        s = '['
+        for g in self.chromosome:
+            s += '1,' if g else '0,'
+        s = s[:-1]+f'] = {self.value} ({self.weight})'
+        return s
+
+    def __repr__(self):
+        return str(self)
