0c55b39b392826632edc229a7f04364a8dcb3b95
youn1on <a.shymanska.ip13@kpi.ua>
2023-01-16 11:26:00 +0200

    Додала візуалізацію згенерованих даних та виведення результатів

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from output import *
 
 if __name__ == "__main__":
     item_set = generate_items(100, 2, 20, 1, 10)
+    print_item_set(item_set)
     start_population = generate_start_population(item_set)
+    print_start_populatiion(start_population)
     ga = GeneticAlgo(start_population, 200)
     best, worst = [], []
     for i in range(10000):
         ga.run_iteration()
         best.append(ga.population[ga.best].value)
         worst.append(ga.population[ga.worst].value)
-    print(ga.population[ga.best].value, ga.population[ga.worst].value)
+    iteration = best.index(max(best))
+    print_best(ga.population[ga.best], item_set, iteration)
     show_statistics(best, worst)
 
 
--- output.py
+++ output.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from bag_item import BagItem
+from creature import Creature
 
 
 def show_statistics(b, w):
@@ -6,3 +8,26 @@ def show_statistics(b, w):
     plt.plot(y_values, b, 'g')
     plt.plot(y_values, w, 'r')
     plt.show()
+
+
+def print_item_set(item_set: list[BagItem]):
+    print("Доступнi предмети:")
+    for i in range(len(item_set)):
+        item = item_set[i]
+        print(f"Предмет {i} Цінність: {item.value} Вага: {item.weight}")
+    print()
+
+
+def print_start_populatiion(start_population: list[Creature]):
+    for i in range(len(start_population)):
+        print(f"Особина {i}: цінність: {start_population[i].value}, вага: {start_population[i].weight }")
+    print()
+
+
+def print_best(best: Creature, item_set: list[BagItem], iteration: int):
+    print(f"Найкраща вартість: {best.value}, вага найкращої особини: {best.weight}")
+    print("Обрані предмети:")
+    for item in [item_set[i] for i in range(len(item_set)) if best.chromosome[i]]:
+        print(f"Цінність: {item.value} Вага: {item.weight}")
+    print(f"Ітерація, на якій було знайдено найкраще значення: {iteration}")
+    print()
